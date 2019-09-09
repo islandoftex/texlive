927445f48dd4cd59c251e5701e333ea699049d01
Ben Frank <ben.frank@mail.de>
2019-09-09 15:52:43 +0200

    Add another layer for historical images
    As of this commit the generation of latest images and historical
    images is separated. The former are built if `$LATESTRELEASES` is set
    to `true` and the latter if `$HISTORICALRELEASES` is set to
    `true`. That way we can select what to build by using CI variables.
    
    Conceptually this commit changes the build flow of historical
    images. Instead of every flavor downloading the full image (that means
    4 downloads of the ISO per release which is extremely load-intensive)
    we are caching the extracted ISO in a docker image on its own. That
    image is pushed as `TL{YEAR}-historic-iso` and used by all images that
    build from the ISO installation.
    
    Unfortunately, GitLab has no easy way to remove this image after the
    build, so the ISO images are cached forever, although they are
    essentially useless for everyone else.

--- create-historical-jobs.sh
+++ create-historical-jobs.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
+# for some reason GitLab CI doesn't accept the `test` jobs to have
+# needs: ["build:historic:$year"]…
+
 year=$((`date +%Y`-1))
 while [ $year -gt 2013 ]
 do
 		echo "`cat <<EOF
+build:iso:$year:
+  variables:
+    CURRENTRELEASE: $year
+  <<: *isodefinition
 build:historic:$year:
-  stage: build
   variables:
     CURRENTRELEASE: $year
     DOCFILES: "no"
     SRCFILES: "no"
   <<: *historicdefinition
 test:historic:$year:
-  stage: test
-  image: registry.gitlab.com/islandoftex/images/texlive:TL$year-historic
-  needs: ["build:historic:$year"]
+  image: \\$RELEASE_IMAGE:TL$year-historic
   <<: *testdefinition
+  only:
+    variables:
+      - \\$HISTORICALRELEASES == "true"
 build:historic:$year-doc:
-  stage: extrabuild
-  needs: ["build:baseimage"]
+  needs: ["build:iso:$year"]
   variables:
     CURRENTRELEASE: $year
     DOCFILES: "yes"
     SRCFILES: "no"
   <<: *historicdefinition
 build:historic:$year-src:
-  stage: extrabuild
-  needs: ["build:baseimage"]
+  needs: ["build:iso:$year"]
   variables:
     CURRENTRELEASE: $year
     DOCFILES: "no"
     SRCFILES: "yes"
   <<: *historicdefinition
 build:historic:$year-doc-src:
-  stage: extrabuild
-  needs: ["build:baseimage"]
+  needs: ["build:iso:$year"]
   variables:
     CURRENTRELEASE: $year
     DOCFILES: "yes"

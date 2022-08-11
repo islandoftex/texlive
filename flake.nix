0a9fb92dbc5f96218b37f50168c5a5bc5bc1d0c3
Ben Frank <ben.frank@mail.de>
2022-08-11 16:42:40 +0200

    Add pre-commit stage to CI (via nix)

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "TeX Live images";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # install pre-commit into the shell environment
+            pre-commit
+            # ... and all its dependencies for the hooks
+            hadolint
+            nixpkgs-fmt
+            shellcheck
+            shfmt
+          ];
+        };
+      }
+    );
+}

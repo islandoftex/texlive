d953ce5d46e2a1abc4c62005e12c6c604c1bfe10
Vít Novotný <witiko@gmail.com>
2022-10-04 11:04:21 +0000

    remove -f from pruning

--- runner-setup/prune-texlive-images.sh
+++ runner-setup/prune-texlive-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker images | grep texlive | awk 'BEGIN {OFS=""} /./ {print $3}' | xargs --no-run-if-empty docker rmi -f
+docker images | grep texlive | awk 'BEGIN {OFS=""} /./ {print $3}' | xargs --no-run-if-empty docker rmi
 docker system prune --force

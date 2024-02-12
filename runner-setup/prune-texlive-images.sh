71710c2ed56ee9a60c78ed191a8b4452e02b05cf
Marei (peiTeX) <marei@peitex.de>
2024-02-12 10:09:33 +0100

    add cleaning of dangling volumes

--- runner-setup/prune-texlive-images.sh
+++ runner-setup/prune-texlive-images.sh
@@ -2,3 +2,6 @@
 
 docker images | grep texlive | awk 'BEGIN {OFS=""} /./ {print $3}' | xargs --no-run-if-empty docker rmi
 docker system prune --force
+
+# also clean dangling volumes
+[ -z "$(docker volume ls -qf dangling=true)"] || docker volume rm $(docker volume ls -qf dangling=true)

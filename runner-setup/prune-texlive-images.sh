ed65e41baebf5173ffedc1d6a69818f05f136bec
Marei Peischl <marei@peitex.de>
2024-03-15 10:58:10 +0000

    Kill leftover buildx containers

--- runner-setup/prune-texlive-images.sh
+++ runner-setup/prune-texlive-images.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# kill containers older than 3 days
+docker ps -f status=running --format "{{.ID}} {{.CreatedAt}} {{.Image}}" | grep buildx | while read -r id cdate ctime _; do if [[ $(date +%s -d "$cdate $ctime") -lt $(date +%s -d '3 days ago') ]]; then docker kill "$id"; fi; done
+
 docker images | grep texlive | awk 'BEGIN {OFS=""} /./ {print $3}' | xargs --no-run-if-empty docker rmi
 docker system prune --force
 
 # also clean dangling volumes
-[ -z "$(docker volume ls -qf dangling=true)" ] || docker volume rm "$(docker volume ls -qf dangling=true)"
+for v in $(docker volume ls -qf 'dangling=true'); do docker volume rm "$v"; done

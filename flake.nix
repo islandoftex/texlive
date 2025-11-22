3d75ff2709db0443602ed24a164e7700e5aebc86
Ben Frank <ben.frank@mail.de>
2025-11-22 19:00:31 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "TeX Live images";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
